c31460182031d5fa50ab1ebfbdd9a19263f9e4b5
gordonduan8 <gordonduan8@gmail.comgit config --global user.email gordonduan8@gmail.com>
2022-09-17 17:43:44 -0400

    gonna get deleted anyways

--- src/app.js
+++ src/app.js
@@ -32,6 +32,6 @@ app.post('/input', (req, res) => {
   });
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
